49f63d7450501fd4d91d21d40b4020b8d9fe25a1
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-10-26 11:24:13 -0300

    Basic Window + Sounds

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import pygame
+import random
+from pygame import *
+
+pygame.init()
+
+scr_size = (width,height) = (600,150)
+FPS = 60
+gravity = 0.6
+
+black = (0,0,0)
+white = (255,255,255)
+background_col = (235,235,235)
+
+high_score = 0
+
+screen = pygame.display.set_mode(scr_size)
+clock = pygame.time.Clock()
+pygame.display.set_caption("Dino Run Game")
