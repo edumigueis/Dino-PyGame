6d680a6081282fc9c650431fcc485979878cbfa4
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-10-28 09:24:41 -0300

    Intro Screen

--- main.py
+++ main.py
@@ -263,3 +263,29 @@ class Cloud(pygame.sprite.Sprite):
         self.rect = self.rect.move(self.movement)
         if self.rect.right < 0:
             self.kill()
+
+class Scoreboard():
+    def __init__(self,x=-1,y=-1):
+        self.score = 0
+        self.tempimages,self.temprect = load_sprite_sheet('numbers.png',12,1,11,int(11*6/5),-1)
+        self.image = pygame.Surface((55,int(11*6/5)))
+        self.rect = self.image.get_rect()
+        if x == -1:
+            self.rect.left = width*0.89
+        else:
+            self.rect.left = x
+        if y == -1:
+            self.rect.top = height*0.1
+        else:
+            self.rect.top = y
+
+    def draw(self):
+        screen.blit(self.image,self.rect)
+
+    def update(self,score):
+        score_digits = extractDigits(score)
+        self.image.fill(background_col)
+        for s in score_digits:
+            self.image.blit(self.tempimages[s],self.temprect)
+            self.temprect.left += self.temprect.width
+        self.temprect.left = 0
