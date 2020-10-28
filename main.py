404490f078d5622020435e28545cf010297dbbcd
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-10-28 09:21:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -85,3 +85,29 @@ def load_sprite_sheet(
     sprite_rect = sprites[0].get_rect()
 
     return sprites, sprite_rect
+
+def disp_gameOver_msg(retbutton_image,gameover_image):
+    retbutton_rect = retbutton_image.get_rect()
+    retbutton_rect.centerx = width / 2
+    retbutton_rect.top = height*0.52
+
+    gameover_rect = gameover_image.get_rect()
+    gameover_rect.centerx = width / 2
+    gameover_rect.centery = height*0.35
+
+    screen.blit(retbutton_image, retbutton_rect)
+    screen.blit(gameover_image, gameover_rect)
+
+def extractDigits(number):
+    if number > -1:
+        digits = []
+        i = 0
+        while(number/10 != 0):
+            digits.append(number%10)
+            number = int(number/10)
+
+        digits.append(number%10)
+        for i in range(len(digits),5):
+            digits.append(0)
+        digits.reverse()
+        return digits
