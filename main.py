448037f2fed3b3994310d306c885146442444698
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-10-28 09:17:41 -0300

    Image Loader

--- main.py
+++ main.py
@@ -19,3 +19,27 @@ high_score = 0
 screen = pygame.display.set_mode(scr_size)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Dino Run Game")
+
+jump_sound = pygame.mixer.Sound('assets/jump.wav')
+die_sound = pygame.mixer.Sound('assets/die.wav')
+checkPoint_sound = pygame.mixer.Sound('assets/checkPoint.wav')
+
+def load_image(
+    name,
+    sizex=-1,
+    sizey=-1,
+    colorkey=None,
+    ):
+
+    fullname = os.path.join('assets', name)
+    image = pygame.image.load(fullname)
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+
+    if sizex != -1 or sizey != -1:
+        image = pygame.transform.scale(image, (sizex, sizey))
+
+    return (image, image.get_rect())
