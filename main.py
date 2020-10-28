36d76b7e11fee9893b32f2407f48f1e3c859d42f
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-10-28 09:23:18 -0300

    Cactus and ground

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def extractDigits(number):
             digits.append(0)
         digits.reverse()
         return digits
-        
+
 def disp_gameOver_msg(retbutton_image,gameover_image):
     retbutton_rect = retbutton_image.get_rect()
     retbutton_rect.centerx = width / 2
@@ -181,3 +181,21 @@ class Dino():
                     checkPoint_sound.play()
 
         self.counter = (self.counter + 1)
+
+class Cactus(pygame.sprite.Sprite):
+    def __init__(self,speed=5,sizex=-1,sizey=-1):
+        pygame.sprite.Sprite.__init__(self,self.containers)
+        self.images,self.rect = load_sprite_sheet('cacti-small.png',3,1,sizex,sizey,-1)
+        self.rect.bottom = int(0.98*height)
+        self.rect.left = width + self.rect.width
+        self.image = self.images[random.randrange(0,3)]
+        self.movement = [-1*speed,0]
+
+    def draw(self):
+        screen.blit(self.image,self.rect)
+
+    def update(self):
+        self.rect = self.rect.move(self.movement)
+
+        if self.rect.right < 0:
+            self.kill()
