2d8778cf9873f4e6d5168c7702230a3be0114226
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-10-28 09:20:34 -0300

    Sprite sheet loader

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from pygame import *
 
 pygame.init()
 
-scr_size = (width,height) = (600,150)
+scr_size = (width, height) = (600, 150)
 FPS = 60
 gravity = 0.6
 
-black = (0,0,0)
-white = (255,255,255)
-background_col = (235,235,235)
+black = (0, 0, 0)
+white = (255, 255, 255)
+background_col = (235, 235, 235)
 
 high_score = 0
 
@@ -24,12 +24,13 @@ jump_sound = pygame.mixer.Sound('assets/jump.wav')
 die_sound = pygame.mixer.Sound('assets/die.wav')
 checkPoint_sound = pygame.mixer.Sound('assets/checkPoint.wav')
 
+
 def load_image(
     name,
     sizex=-1,
     sizey=-1,
     colorkey=None,
-    ):
+):
 
     fullname = os.path.join('assets', name)
     image = pygame.image.load(fullname)
@@ -43,3 +44,44 @@ def load_image(
         image = pygame.transform.scale(image, (sizex, sizey))
 
     return (image, image.get_rect())
+
+
+def load_sprite_sheet(
+        sheetname,
+        nx,
+        ny,
+        scalex=-1,
+        scaley=-1,
+        colorkey=None,
+):
+    fullname = os.path.join('sprites', sheetname)
+    sheet = pygame.image.load(fullname)
+    sheet = sheet.convert()
+
+    sheet_rect = sheet.get_rect()
+
+    sprites = []
+
+    sizex = sheet_rect.width/nx
+    sizey = sheet_rect.height/ny
+
+    for i in range(0, ny):
+        for j in range(0, nx):
+            rect = pygame.Rect((j*sizex, i*sizey, sizex, sizey))
+            image = pygame.Surface(rect.size)
+            image = image.convert()
+            image.blit(sheet, (0, 0), rect)
+
+            if colorkey is not None:
+                if colorkey is -1:
+                    colorkey = image.get_at((0, 0))
+                image.set_colorkey(colorkey, RLEACCEL)
+
+            if scalex != -1 or scaley != -1:
+                image = pygame.transform.scale(image, (scalex, scaley))
+
+            sprites.append(image)
+
+    sprite_rect = sprites[0].get_rect()
+
+    return sprites, sprite_rect
